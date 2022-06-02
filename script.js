45cbf31c4fcf0feef1d3c3139ffd5b064edd3964
FranciskaDendooven <franciska.dendooven@gmail.com>
2022-06-02 17:01:00 +0200

    Add alert when clicking the list

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 const ulEl = document.querySelector("ul");
-/* for (let i = 0; i < ulEl.children.length; i++){
+const items = document.querySelectorAll("li");
+for (let i = 0; i < ulEl.children.length; i++){
     console.log(ulEl.children[i].innerHTML);
-} */
+}
+ulEl.addEventListener ("click", event => {
+    if (ulEl.children[0]==ulEl.children[0])
+    alert(ulEl.children[0].innerText);
+
+    else (ulEl.children[1]==ulEl.children[1])
+    alert(ulEl.children[1].innerText);
+});
 
 ulEl.prepend(ulEl.children[5], ulEl.children[0]);
 console.log(ulEl.children.innerText);
 
 ulEl.children[0].className = "important";
+
